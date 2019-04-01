0b4b2f56b87012d064bf399369e3d0c181b63dba
Faiz Akram <faiz.akram@compunnel.in>
2019-04-01 12:02:52 +0530

    commit

--- src/main/java/com/st/s3/KeyContant.java
+++ src/main/java/com/st/s3/KeyContant.java
@@ -3,8 +3,8 @@ package com.st.s3;
 public class KeyContant {
 	private KeyContant() {
 	}
-	public static final String ACCESS_KEY ="AKIATG7TGEXJQ6JXPHD2"; 
-	public static final String SECRET_KEY ="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"; 
-	public static final String BUCKET_NAME= "my-first-app-online";
+	public static final String ACCESS_KEY =""; 
+	public static final String SECRET_KEY =""; 
+	public static final String BUCKET_NAME= "";
 	public static final String PATH_NAME= "<PATH_NAME>";
 }
